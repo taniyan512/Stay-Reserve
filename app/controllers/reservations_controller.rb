5306dc5d4f417dcf1f26ead72ef9093a6be51ef0
谷田匠 <tanidatakumi@tanitakunettowaku.local>
2022-05-10 07:27:14 +0900

    :idの受け取り方について

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -3,8 +3,12 @@ class ReservationsController < ApplicationController
   end
 
   def confirm
+    @room = Room.find(params[:id])
     @reservation = Reservation.new(params.permit(:startDay, :endDay, :room_id, :numberOfPeople))
     @reservation.user_id = current_user.id
+    @days = (@reservation.endDay - @reservation.startDay).to_i
+    @reservation.totalPrice = @days * @reservation.numberOfPeople * @room.price
+    
   end
 
   def new
