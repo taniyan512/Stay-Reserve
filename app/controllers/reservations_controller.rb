2e7141b63a84b4cbff16e02f54a18f7a6be13d4d
谷田匠 <tanidatakumi@tanitakunettowaku.local>
2022-05-11 06:21:46 +0900

    reservationコントローラのshowアクションのエラーについて

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -3,8 +3,8 @@ class ReservationsController < ApplicationController
   end
 
   def confirm
-    @room = Room.find(params[:id])
-    @reservation = Reservation.new(params.permit(:startDay, :endDay, :room_id, :numberOfPeople))
+    @room = Room.find(params[:room_id])
+    @reservation = Reservation.new(params.permit(:startDay, :endDay, :room_id, :numberOfPeople, :totalPrice))
     @reservation.user_id = current_user.id
     @days = (@reservation.endDay - @reservation.startDay).to_i
     @reservation.totalPrice = @days * @reservation.numberOfPeople * @room.price
@@ -16,7 +16,17 @@ class ReservationsController < ApplicationController
   end
 
   def create
-    
-   
+    @reservation = Reservation.new(params.require(:reservation).permit(:startDay, :endDay, :room_id, :numberOfPeople, :totalPrice))
+    @room = Room.find(params[:reservation][:room_id])
+    @reservation.user_id = current_user.id
+    if @reservation.save
+      flash[:notice] = "Reservation was successfully created."
+      redirect_to reservation_path(@reservation)
+    end
+  end
+
+  def show
+    @reservation = Reservation.find(params[:id])
+    @room = Room.find(params[:reservation][:room_id])
   end
 end
