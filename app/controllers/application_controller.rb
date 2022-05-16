34df2c42defe47c4c4c22d447918a55d72547316
谷田匠 <tanidatakumi@tanitakunettowaku.local>
2022-05-16 20:34:15 +0900

    application.controller.rbを編集したが引き続きエラー発生

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,21 +2,20 @@ class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
-
-  def configure_permitted_parameters
+  
+  def configure_permitted_parameters    
     added_attrs = [ :username,
                     :email,
                     :password,
                     :password_confirmation,
+                    :user_image,
+                    :self_introduce,
                   ]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
     devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
   end
 
- def after_sign_in_path_for(resource)
-    profile_home_path                            
- end
 
 
 end
